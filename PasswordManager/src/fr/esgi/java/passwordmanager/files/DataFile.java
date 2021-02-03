71b5b68be0a1b91b98897ea0110b31a866e833cc
Claraflo <clara.florac@gmail.com>
2021-02-03 19:59:06 +0100

    Classes datafile et userfile clean

--- PasswordManager/src/fr/esgi/java/passwordmanager/files/DataFile.java
+++ PasswordManager/src/fr/esgi/java/passwordmanager/files/DataFile.java
@@ -2,32 +2,53 @@ package fr.esgi.java.passwordmanager.files;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
+
+/**Class DataFile
+ * Represante the dataFile of one user.
+ **/
 
 public class DataFile {
 
     private final String username;
     public File file;
 
+
     public DataFile(String username) {
         this.username = username.substring(0, 1).toUpperCase() + username.substring(1);
         try {
             this.file = new File("src\\fr\\esgi\\java\\passwordmanager\\json\\" + this.username + "Data.json").getAbsoluteFile();
         } catch (Exception exceptionFileNotFound) {
-            System.out.println("Le fichier de donnee utilisateur est introuvable. L'application ne peut fonctionner sans.");
-            System.exit(1);
+            System.out.println("Le fichier de donnee utilisateur est introuvable. Un nouveau fichier donne utilisateur est cree. Les anciennes informations sont perdues.");
+            creatDataFile(this.username);
         }
     }
 
-    public String getUsername(){
+
+    public void creatDataFile(String userName) {
+
+        String username = userName.substring(0, 1).toUpperCase() + userName.substring(1);
+        try {
+            File newDataFile = new File("src\\fr\\esgi\\java\\passwordmanager\\json\\" + username + "Data.json").getAbsoluteFile();
+            newDataFile.createNewFile();
+            PrintWriter writer = new PrintWriter(newDataFile.getAbsoluteFile(), "UTF-8");
+            writer.println("[]");
+            writer.close();
+        } catch (Exception e) {
+            System.out.println("Impossible d'écrire dans le dossier d'utilisateurs nomme json.");
+        }
+    }
+
+    public String getUsername() {
         return this.username;
     }
 
     public String fileToString() throws IOException {
-        String filename = "src\\fr\\esgi\\java\\passwordmanager\\json\\"+ this.username + "Data.json";
+        String filename = "src\\fr\\esgi\\java\\passwordmanager\\json\\" + this.username + "Data.json";
         String jsonString = "";
 
         StringBuilder builder = new StringBuilder();
@@ -37,7 +58,7 @@ public class DataFile {
         return builder.toString();
     }
 
-    public String path(){
-        return "json\\"+ this.username + "Data.json";
+    public String path() {
+        return "src\\fr\\esgi\\java\\passwordmanager\\json\\" + this.username + "Data.json";
     }
 }
--- PasswordManager/src/fr/esgi/java/passwordmanager/files/UserFile.java
+++ PasswordManager/src/fr/esgi/java/passwordmanager/files/UserFile.java
@@ -2,24 +2,44 @@ package fr.esgi.java.passwordmanager.files;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
+
+/**
+ * Class UserFile
+ * Represante the userFile containing the list of users of this application.
+ **/
 
 public class UserFile {
 
     private String filename;
     public File file;
 
-    public UserFile(){
+    public UserFile() {
         this.filename = "src\\fr\\esgi\\java\\passwordmanager\\json\\UserFile.json";
-        try{
+        try {
             this.file = new File("src\\fr\\esgi\\java\\passwordmanager\\json\\UserFile.json").getAbsoluteFile();
 
-        }catch (Exception exceptionFileNotFound){
-            System.out.println("Le fichier de configuration utilisateur est introuvable. L'application ne peut fonctionner sans.");
-            System.exit(1);
+        } catch (Exception exceptionFileNotFound) {
+            System.out.println("Le fichier de configuration utilisateur est introuvable. Un nouveau fichier est genere. Les anciens utilisateurs sont perdus.");
+            creatDataFile();
+        }
+    }
+
+
+    public void creatDataFile() {
+
+        try {
+            File newDataFile = new File("src\\fr\\esgi\\java\\passwordmanager\\json\\UserFile.json").getAbsoluteFile();
+            newDataFile.createNewFile();
+            PrintWriter writer = new PrintWriter(newDataFile.getAbsoluteFile(), "UTF-8");
+            writer.println("[]");
+            writer.close();
+        } catch (Exception e) {
+            System.out.println("Impossible d'écrire dans le dossier d'utilisateurs nomme json.");
         }
     }
 
