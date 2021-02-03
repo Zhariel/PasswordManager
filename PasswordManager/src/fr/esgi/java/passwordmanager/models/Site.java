da8ac480df218ed5999fbde69fe81593917cb073
Claraflo <clara.florac@gmail.com>
2021-02-03 19:43:57 +0100

    Classe Site clean

--- PasswordManager/src/fr/esgi/java/passwordmanager/models/Site.java
+++ PasswordManager/src/fr/esgi/java/passwordmanager/models/Site.java
@@ -4,8 +4,13 @@ package fr.esgi.java.passwordmanager.models;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
+/**
+ * Class Site
+ * This class contains name,password,idUser,constraints and metaData.
+ **/
 
 public class Site {
+
     private String name;
     private Password password;
     private String idUser;
@@ -13,7 +18,11 @@ public class Site {
     public Metadata metaData;
 
 
-    public Site(String name, Password password, String idUserInSite, Constraint constraint, Metadata metaData){
+    /**
+     * Constructor
+     * It's used when a site is created from fileData
+     **/
+    public Site(String name, Password password, String idUserInSite, Constraint constraint, Metadata metaData) {
         this.name = name;
         this.password = password;
         this.idUser = idUserInSite;
@@ -21,28 +30,31 @@ public class Site {
         this.metaData = metaData;
     }
 
-
-    public Site(ArrayList<String> listInputsFormCreatSite){
+    /**
+     * Constructor
+     * It's used when a site is created from inputs user.
+     **/
+    public Site(ArrayList<String> listInputsFormCreatSite) {
 
         this.name = listInputsFormCreatSite.get(0);
         this.idUser = listInputsFormCreatSite.get(7);
 
-        if(listInputsFormCreatSite.get(1).equals("y")){
-            this.constraint = new Constraint(listInputsFormCreatSite.subList(2,7));
-        }else{
-            this.constraint = new Constraint(); //default constructor set values total : 8, Upper : 1, lower : 1, speChar : 1, digit : 1, letters : 4
+        if (listInputsFormCreatSite.get(1).equals("y")) {
+            this.constraint = new Constraint(listInputsFormCreatSite.subList(2, 7));
+        } else {
+            this.constraint = new Constraint(); //default constructor set values totalLength : 8, Upper : 1, lower : 1, speChar : 1, digit : 1, letters : 4
         }
 
         //User chooses to entry a password (entry yes)
-        if(listInputsFormCreatSite.get(8).equals("y")){
-            this.password = new Password(listInputsFormCreatSite.get(9),this.constraint,false);
-        }else{
+        if (listInputsFormCreatSite.get(8).equals("y")) {
+            this.password = new Password(listInputsFormCreatSite.get(9), this.constraint, false);
+        } else {
             //Application generates the password.
-            this.password = new Password(this.constraint,false);
+            this.password = new Password(this.constraint, false);
         }
 
         LocalDate date = LocalDate.now();
-        this.metaData = new Metadata(date,listInputsFormCreatSite.get(13),listInputsFormCreatSite.get(11));
+        this.metaData = new Metadata(date, listInputsFormCreatSite.get(13), listInputsFormCreatSite.get(11));
 
     }
 
@@ -55,7 +67,9 @@ public class Site {
     }
 
 
-    public String getPassword() { return password.getPassword(); }
+    public String getPassword() {
+        return password.getPassword();
+    }
 
     public void setPassword(String newPassword) {
         this.password.setPassword(newPassword);
